a43aeb33650c429fd92a279a8ac2fac600df9f80
ognie <skaymorik@gmail.com>
2021-11-05 10:35:38 +0200

    hw9 v 1.1

--- src/main/java/hw9/tasks/task1/Main.java
+++ src/main/java/hw9/tasks/task1/Main.java
@@ -1,9 +1,7 @@
 package hw9.tasks.task1;
 
-import java.io.File;
-
 public class Main {
-    private static final String FILE_TASK1 = "C:\\Users\\skyog\\IdeaProjects\\hw-refresh\\src\\main\\java\\hw9\\resources\\file1\\file.txt";
+    private static final String FILE_TASK1 = "src/main/java/hw9/resources/file1/file.txt";
 
     public static void main(String[] args) {
         Validation.ValidPhone(FILE_TASK1);
